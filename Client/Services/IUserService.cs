4a40a16e2690fbe74b3f3676d0ec22555c4618ba
Laurynas <lauris.gailius@gmail.com>
2023-06-15 15:29:20 +0300

    Added UserService

---
+++ Client/Services/IUserService.cs
@@ -0,0 +1,8 @@
+﻿namespace Rock_Paper_Scissors.Client.Services
+{
+    public interface IUserService
+    {
+        public string Username { get; set; }
+        public string RoomCode { get; set; }
+    }
+}
---
+++ Client/Services/UserService.cs
@@ -0,0 +1,8 @@
+﻿namespace Rock_Paper_Scissors.Client.Services
+{
+    public class UserService : IUserService
+    {
+        public string Username { get; set; }
+        public string RoomCode { get; set; }
+    }
+}
