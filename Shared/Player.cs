288f5e6916378b0122fc9139975c0cd2fd63084f
Laurynas <lauris.gailius@gmail.com>
2023-06-15 20:48:35 +0300

    Added player object to be sent between server and client

---
+++ Shared/Player.cs
@@ -0,0 +1,31 @@
+﻿namespace Rock_Paper_Scissors.Shared
+{
+    public class Player
+    {
+        public string? Username { get; set; }
+
+        public string? RoomCode { get; set; }
+
+        public string? Choice { get; set; }
+
+        public string? ConnectionId { get; set; }
+
+        public Player(string username, string roomCode, string connectionId)
+        {
+            this.Username = username;
+            this.RoomCode = roomCode;
+            this.ConnectionId = connectionId;
+        }
+
+        public Player(string username, string roomCode)
+        {
+            this.Username = username;
+            this.RoomCode = roomCode;
+        }
+
+        public Player()
+        {
+
+        }
+    }
+}
